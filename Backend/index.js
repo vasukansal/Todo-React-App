ba8835c38a9497cb30e4c0336c83c81793f4bcf4
vasukansal <vasu12b8075@gmail.com>
2024-04-24 10:19:54 +0530

    removed cors

--- Backend/index.js
+++ Backend/index.js
@@ -3,9 +3,9 @@ const app = express();
 const { createtodo } = require("./types");
 const { completetodo } = require("./types");
 const { todo } = require("./db");
-const cors=require('cors')
+// const cors=require('cors')
 require("dotenv").config()
-app.use(cors())
+// app.use(cors())
 app.use(express.json());
 const PORT =process.env.PORT || 3000
 //inputs-                              FOR ZOD VALIDATION( SO WE WILL CREATE A NEW FILE FOR ZOD VALIDATION NAMED TYPE.JS )
